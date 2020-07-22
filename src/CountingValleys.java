ccc059d2aa0a713394563bef58fe12fb380c7c83
Artem Kotov <artem.kotov@leovegas.com>
2020-07-22 19:01:18 +0300

    Counting valleys

---
+++ src/CountingValleys.java
@@ -0,0 +1,29 @@
+public class CountingValleys {
+    static int countingValleys(int n, String s) {
+        int valleysCount = 0;
+        int stepsUpAtSeaLevel = 0;
+        int stepsDownAtSeaLevel = 0;
+        boolean isValleyStarted = false;
+        for (String step: s.split("(?!^)")) {
+            if (step.equalsIgnoreCase("u")) {
+                stepsUpAtSeaLevel++;
+            } else {
+                stepsDownAtSeaLevel++;
+            }
+            if (stepsDownAtSeaLevel == 1 && stepsUpAtSeaLevel == 0 && !isValleyStarted) {
+                valleysCount++;
+                isValleyStarted = true;
+            }
+            if (stepsUpAtSeaLevel == stepsDownAtSeaLevel) {
+                stepsUpAtSeaLevel = 0; stepsDownAtSeaLevel = 0;
+                isValleyStarted = false;
+            }
+        }
+        return valleysCount;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Expected 1: " + countingValleys(8, "UDDDUDUU"));
+        System.out.println("Expected 2: " + countingValleys(12, "DDUUDDUDUUUD"));
+    }
+}
